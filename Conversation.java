04908abe87a88d8623e8f320aec4197298a87148
Adline Dely <adely@smith.edu>
2026-02-17 21:00:43 -0500

    commit

--- Conversation.java
+++ Conversation.java
@@ -1,28 +1,63 @@
 // You should **not** update any call signatures in this file
 // only modify the body of each function
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
 class Conversation implements ConversationRequirements {
 
   // Attributes 
+  int numRounds;
+  ArrayList<String> transcript;
 
   /**
    * Constructor 
    */
-  Conversation() {
-    
+  public Conversation() {
+    this.numRounds = 100;
+    this.transcript = new ArrayList<String>();
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
-
+    // this a scanner object
+    Scanner input = new Scanner(System.in);
+    // this is to set the round to 0 
+    System.out.println("How many rounds?");
+    this.numRounds = input.nextInt();
+    input.nextLine(); // don't want the number to be read into what's next
+    int round = 0;
+  // System.in is take an input and System.out is to print out
+  // this print out the first prompt. this prompt is out of the loop
+  // because I don't want this prompt to appear over and over
+  String greeting = "Hi there! What's your mind?";  
+  System.out.println(greeting);
+  // this will save the previous string 
+  this.transcript.add(greeting);
+      while (round < this.numRounds){
+  // get the user input 
+      String answer = input.nextLine();
+      this.transcript.add(answer); // save user input 
+      String Response = respond(answer);
+      System.out.println(Response);
+      this.transcript.add(Response); // Save computer input 
+      round = round + 1;
+    }
+    String goodbye = "See ya!";
+    System.out.println(goodbye);
+    this.transcript.add(goodbye);
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+  System.out.println("\nTRANSCRIPT:");
+  for (String line : this.transcript){
+    System.out.println(line);
+  }
   }
 
   /**
@@ -31,8 +66,36 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+    Random random = new Random();
+  // canned responses 
+    String [] cannedResponses = {
+      "Mmmm-hmm. ",
+      "Tell me more. ",
+      "Interesting, go on.",
+      "I see.",
+      "Why do you say that?"
+    };
+  // this will start with the user's string 
+    String returnString = inputString; 
+  // this will mirror the user answer to turn into a question that the chatbot will ask
+      returnString = returnString.replace("I am", "Are you");
+      returnString = returnString.replace("I'm", "You're");
+      returnString = returnString.replace("am I", "you are");
+      returnString = returnString.replace("my", "salut");
+      returnString = returnString.replace("your", "my");
+      returnString = returnString.replace("salut", "your");
+      returnString = returnString.replace("I", "you");
+      returnString = returnString.replace("me", "you");
+  // this will check if anything actually change ?
+  if (returnString.equals(inputString)) {
+    // this is when nothing was mirrored
+    int index = random.nextInt(cannedResponses.length);
+    return cannedResponses[index];
+  } else {
+    // something change so return the mirrored version 
+    return returnString + "?";
+  }
+
   }
 
   public static void main(String[] arguments) {
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+I forgot their name but it's one of my classmate ! They usally sits in the back same row as me. 
 - Which session(s) of TA / office hours did you attend?
+None. I was thinking of going to TA but I was told it wasn't as helpful so instead I did some searching of my own and use oracle docs. 
 - What are your initial impressions of Java? 
+Java is entire different beast but like python, I just need to give it a chance. I know eventually I'll get faster at completing these assignments and it will make more sense as well! For now, it feels like it make sense but sometimes feels like it's just magic. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+One main difference, base on what I know, is that you have protection in java than python. You can have something be turn into private. Python, on the other hand, is a lot easier to learn and the components of a coding line is a lot fewers. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+Starting early was very helpful, taking some class time to talk about it was very helpful ! One thing that I struggle with is getting myself unstuck. 
